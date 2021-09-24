655b96c9adafaccd3cf35f48760d0545bdaf7771
ele4327 <sebasgarzons88@gmail.com>
2021-09-23 21:30:03 -0500

    Functions Index III

---
+++ functions_d.c
@@ -0,0 +1,59 @@
+#include "monty.h"
+
+void _add(stck_b **header, unsigned int line_number)
+{
+    if (*header == NULL || (*header)->next == NULL)
+    {
+        printf("L%u: can't add, stack too short\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+
+    (*header)-> next-> x += (*header)-> x;
+    (*header) = (*header) -> next;
+    free((*header) -> prev);
+    (*header) -> prev = NULL;
+}
+
+void _sub(stck_b **header, unsigned int line_number)
+{
+    if (*header == NULL || (*header)->next == NULL)
+    {
+        printf("L%u: can't sub, stack too short\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+    (*header) -> next -> x -= (*header) -> x;
+    (*header) = (*header) -> next;
+    free((*header) -> prev);
+    (*header) -> prev = NULL;
+}
+
+void _mul(stck_b **header, unsigned int line_number)
+{
+    if (*header == NULL || (*header) -> next == NULL)
+    {
+        printf("L%u: can't mul, stack too short\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+    (*header) -> next -> x *= (*header) -> x;
+    (*header) = (*header) -> next;
+    free((*header) -> prev);
+    (*header) -> prev = NULL;
+}
+
+void _div(stck_b **header, unsigned int line_number)
+{
+    if (*header == NULL || (*header) -> next == NULL)
+    {
+        printf("L%u: can't div, stack too short\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+    if ((*header) -> x == 0 || (*header) -> next -> x == 0)
+    {
+        printf("L%u: division by zero\n", line_number);
+    }
+
+    (*header) -> next -> x /= (*header) -> x;
+    (*header) = (*header) -> next;
+    free((*header) -> prev);
+    (*header) -> prev = NULL;
+}
