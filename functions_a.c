328757dfe29fe460bc61eb6c83f0838e0a748c24
ele4327 <sebasgarzons88@gmail.com>
2021-09-23 20:49:12 -0500

    Index Functions I

---
+++ functions_a.c
@@ -0,0 +1,70 @@
+#include "header.h"
+
+int add_lst_nd(stck_b **header, int x)
+{
+	stck_b *nw_nd;
+
+	if (!header)
+	{
+		return (-1);
+	}
+
+	nw_nd = malloc(sizeof(struct stck_a));
+
+	if (!nw_nd)
+	{
+		printf("Error: malloc failed");
+		return (-1);
+	}
+
+	nw_nd->x = x;
+
+	if (*header == NULL)
+	{
+		*header = nw_nd;
+		nw_nd->next = NULL;
+		nw_nd->prev = NULL;
+	}
+	else
+	{
+		nw_nd->next = *header;
+		(*header)->prev = nw_nd;
+		*header = nw_nd;
+	}
+
+	return (0);
+}
+
+void dlt_lst_nd(stck_b **header)
+{
+	stck_b *dlt = NULL;
+
+	dlt = *header;
+	if ((*header)->next == NULL)
+	{
+		*header = NULL;
+		free(dlt);
+	}
+	else
+	{
+		*header = (*header)->next;
+		(*header)->prev = NULL;
+		free(dlt);
+	}
+}
+
+void free_lst(stck_b **header)
+{
+	if (!header)
+	{
+		return;
+	}
+
+	while (*header && (*header)->next)
+	{
+		*header = (*header)->next;
+		free((*header)->prev);
+	}
+	
+	free(*header);
+}
