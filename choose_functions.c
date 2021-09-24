ae0c56eddceb97f5ce24a6b12d755250d123486a
ele4327 <sebasgarzons88@gmail.com>
2021-09-23 20:49:55 -0500

    Index Functions

---
+++ choose_functions.c
@@ -0,0 +1,27 @@
+#include "header.h"
+
+void (*opfunc(char *tkn_a))(stck_b **stack, unsigned int line_number)
+{
+	strck_op_out strk_op[] = 
+	{
+		{"pall", pall},
+		{"pop", pop},
+		{"pint", pint},
+		{"swap", swap},
+		{"add", _add},
+		{"nop", nop},
+		{NULL, NULL}
+	};
+
+	int x = 0;
+
+	while (strk_op[x].f != NULL)
+	{
+		if (strcmp(tkn_a, strk_op[x].opcode) == 0)
+		{
+			return(strk_op[x].f);
+		}
+		x++;
+	}
+	return (NULL);
+}
